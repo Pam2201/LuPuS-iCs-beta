236041ab9feec3d92e9b602acd8df7cfddae3137
Gareth <garethwilliams21@gmail.com>
2012-12-20 01:15:21 +0000

    Fixed both 480p & 720p recording

--- arch/arm/mach-msm/board-semc_zeus.c
+++ arch/arm/mach-msm/board-semc_zeus.c
@@ -114,12 +114,13 @@
 #define MSM_PMEM_SF_SIZE	0x1E00000
 #define MSM_FB_SIZE		0x500000
 #define MSM_GPU_PHYS_SIZE       SZ_4M
-#define MSM_PMEM_CAMERA_SIZE 0x0000000
 //////////////////////////////////////////////
 // 720p
-#define MSM_PMEM_ADSP_SIZE 0x3800000
+//#define MSM_PMEM_CAMERA_SIZE 0x2000000
+//#define MSM_PMEM_ADSP_SIZE 0x1800000
 // 480p
-//#define MSM_PMEM_ADSP_SIZE 0x1700000
+#define MSM_PMEM_CAMERA_SIZE 0x1700000
+#define MSM_PMEM_ADSP_SIZE 0x0700000
 /////////////////////////////////////////////
 #define PMEM_KERNEL_EBI1_SIZE 0x600000
 #define MSM_PMEM_AUDIO_SIZE 0x200000
